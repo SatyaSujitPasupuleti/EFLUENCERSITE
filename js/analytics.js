a8b35544cbc7fa7de01f1767255f52be3508952f
satyaP <sats.pasupuleti@gmail.com>
2018-08-01 12:44:54 -0400

    second commit

---
+++ js/analytics.js
@@ -0,0 +1,32 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+         legend: {
+        display: false
+    },
+    tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
+        }
+    }
+        
+    }
+});
