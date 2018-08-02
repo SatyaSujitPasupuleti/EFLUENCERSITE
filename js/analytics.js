09402311138a6de1f18f4a892a5eca93eafaa951
satyaP <sats.pasupuleti@gmail.com>
2018-08-02 11:28:12 -0400

    added content to settings and profile

--- js/analytics.js
+++ js/analytics.js
@@ -9,6 +9,7 @@ var chart = new Chart(ctx, {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
             label: "Transactions Made",
+            fill: false,
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45],
@@ -17,16 +18,280 @@ var chart = new Chart(ctx, {
 
     // Configuration options go here
     options: {
-         legend: {
-        display: false
-    },
-    tooltips: {
-        callbacks: {
-           label: function(tooltipItem) {
-                  return tooltipItem.yLabel;
-           }
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
         }
+
     }
-        
+});
+var ctx = document.getElementById('myChart2').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart3').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart4').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart5').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart6').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart7').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart8').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
+    }
+});
+var ctx = document.getElementById('myChart9').getContext('2d');
+Chart.defaults.global.defaultFontColor = 'white';
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Transactions Made",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 1000, 50000, 20000, 200000, 300000, 450000],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem) {
+                    return tooltipItem.yLabel;
+                }
+            }
+        }
+
     }
 });
