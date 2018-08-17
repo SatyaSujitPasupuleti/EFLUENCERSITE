50f1c8360e5a6c59c6a20167611289224a947e40
satyaP <sats.pasupuleti@gmail.com>
2018-08-17 15:27:14 -0400

    updated ui, added elements to page

--- js/test.js
+++ js/test.js
@@ -1,4 +1,12 @@
  //Adds materialize resizable sidenav to all pages
  $(document).ready(function () {
      $('.sidenav').sidenav();
+     $("#button").click(function () {
+         alert("Handler for .click() called.");
+         $("#hideable").toggle();
+     });
+     $(".chat-closed").on("click", function (e) {
+         $(".chat-header,.chat-content").removeClass("hide");
+         $(this).addClass("hide");
+     });
  });
